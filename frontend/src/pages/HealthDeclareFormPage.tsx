018f8ebed7ff5cb1edfb60acabbefd9c2f76ca01
Jacky Lau <jackylqlong@gmail.com>
2025-09-15 01:00:21 +0800

    add validation to declaration form

--- frontend/src/pages/HealthDeclareFormPage.tsx
+++ frontend/src/pages/HealthDeclareFormPage.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, Radio, Stack, TextInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { isNotEmpty, useForm } from '@mantine/form';
 
 interface HealthDeclareFormValues {
   fullName: string;
@@ -10,6 +10,14 @@ interface HealthDeclareFormValues {
 export function HealthDeclareFormPage() {
   const healthDeclareForm = useForm<HealthDeclareFormValues>({
     mode: 'uncontrolled',
+    validate: {
+      fullName: isNotEmpty('Full name is required'),
+      temperature: (value) =>
+        /^\d{2}\.\d{1}$/.test(value)
+          ? null
+          : 'Please enter a valid temperature in degree Celsius (e.g. 36.5)',
+      isInCloseContactCovid: isNotEmpty('Please select an option'),
+    },
   });
 
   const onSubmitHealthDeclaration = (values: HealthDeclareFormValues) => {
@@ -38,10 +46,10 @@ export function HealthDeclareFormPage() {
             label="Have you been in contact with anyone who is
 suspected to have/has been diagnosed with Covid-19 within the last 14 days?"
             withAsterisk
-            key={healthDeclareForm.key('isInCloseContact')}
-            {...healthDeclareForm.getInputProps('isInCloseContact')}
+            key={healthDeclareForm.key('isInCloseContactCovid')}
+            {...healthDeclareForm.getInputProps('isInCloseContactCovid')}
           >
-            <Stack mt="xs" gap="xs">
+            <Stack mt="xs" mb="xs" gap="xs">
               <Radio value="yes" label="Yes" />
               <Radio value="no" label="No" />
             </Stack>
